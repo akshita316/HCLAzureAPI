aaa37b54e5fc06c68a2998703fda771f52fb956c
Sanay Devi <sanaydevi@gmail.com>
2020-07-05 10:24:47 -0500

    Commiting changed html file

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def homePage():
     return render_template('index.html')
 
+# @app.route('/UploadFile',methods="PUT")
+# def uploadFile():
+#
 
 if __name__ == '__main__':
     app.run()
